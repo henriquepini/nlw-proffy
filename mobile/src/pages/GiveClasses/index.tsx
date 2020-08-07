ce4b85007b0427221ee359f5871e6d518b25c4c5
Henrique Pini <henriqueflopi@hotmail.com>
2020-08-07 03:54:44 -0300

    Day #4

---
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { View, ImageBackground, Text, Linking } from 'react-native';
+
+import giveClassesBgImage from '../../assets/images/give-classes-background.png';
+
+import styles from './styles';
+import { RectButton } from 'react-native-gesture-handler';
+
+function GiveClasses() {
+  const URL = "http://192.168.0.109:3000";
+
+  function handleOpenWebPlatform() {
+    Linking.openURL(URL).catch((err) => console.log('An error occurred', err));
+  }
+  return (
+    <View style={styles.container}>
+      <ImageBackground 
+        resizeMode="contain" 
+        source={giveClassesBgImage} 
+        style={styles.content}
+      >
+        <Text style={styles.title}>Quer ser um Proffy ?</Text>
+        <Text style={styles.description}>
+          Para começar, você precisa se cadastrar como professor na nossa plataforma web.
+        </Text>
+      </ImageBackground>
+
+      <RectButton onPress={handleOpenWebPlatform} style={styles.okButton}>
+        <Text style={styles.okButtonText}>Acessar plataforma</Text>
+      </RectButton>
+    </View>
+  );
+}
+
+export default GiveClasses;
